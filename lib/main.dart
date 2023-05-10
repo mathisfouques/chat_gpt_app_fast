950d450c0d62d7c43a761ad8d58dbe21b9523fb7
Mathis Fouques <mathisfouques@MacBook-Pro-de-Mathis-2.local>
2023-05-10 17:44:00 +0200

    simple finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:http/http.dart';
@@ -23,8 +24,6 @@ Future<String> _getNextPrompt(String question) async {
       "max_tokens": 1000,
     }),
   );
-
-  print(jsonDecode(response.body));
 
   return (jsonDecode(response.body)["choices"] as List).first["message"]
       ["content"];
